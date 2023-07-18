ef0032c843f1657e70a4eb67d7e56a48787397d6
Dominus-Rahl <137926077+Dominus-Rahl@users.noreply.github.com>
2023-07-18 23:00:04 +0500

    Add files via upload

---
+++ Seminar6/Task1/Program.cs
@@ -0,0 +1,23 @@
+﻿//Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+//0, 7, 8, -2, -2 -> 2
+//1, -7, 567, 89, 223-> 3
+int Prompt(string message)
+{
+    Console.WriteLine(message);
+    string value = Console.ReadLine();
+    int result = Convert.ToInt32(value);
+    return result;
+}
+
+int size = Prompt("Введите размер массива");
+int[] array = new int[size];
+int count = 0;
+for (int i = 0; i < size; i++)
+{
+    array[i] = Prompt("Введите число");
+    if (array[i] > 0)
+    {
+        count++;
+    }
+}
+Console.WriteLine($"Пользователь ввел {count} чисел");
---
+++ Seminar6/Task2/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+//b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+int Prompt(string message)
+{
+    Console.WriteLine(message);
+    string value = Console.ReadLine();
+    int result = Convert.ToInt32(value);
+    return result;
+}
+
+double b1 = Prompt("Введите b1");
+double k1 = Prompt("Введите k1");
+double b2 = Prompt("Введите b2");
+double k2 = Prompt("Введите k2");
+double x = Math.Round(((b2 - b1) / (k1 - k2)), 3);
+double y1 = Math.Round((k1 * x + b1), 3);
+double y2 = Math.Round((k2 * x + b2), 3);
+Console.WriteLine($"Координаты точки {x} {y1}");
+Console.WriteLine($"Координаты точки {x} {y2}");
